059a4e2778d827b518ef59e72afc9967f0c12caf
bamboo51 <pongsatorn2548self@gmail.com>
2024-03-30 07:52:05 +0900

    Resampling data

---
+++ data/num_file/count_from_01.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+directory = './min_01/'
+
+for filename in sorted(os.listdir(directory)):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(directory, filename)
+
+        df = pd.read_csv(file_path)
+        
+        df['datetime'] = pd.to_datetime(df['datetime'])
+        df.set_index('datetime', inplace=True)
+        result = df.resample('1h').sum()
+        print(result)
+        
+        output_filename = os.path.splitext(filename)[0]
+        print(output_filename)
+        output_path = os.path.join('./hrs_01/', output_filename+'.csv')
+        result.to_csv(output_path)
+        
